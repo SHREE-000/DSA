cccaaf483985267bd013ed27882494542cbb4534
Shreehari C R <shreehari000@gamil.com>
2024-07-26 21:01:07 +0530

    fibonacci series with for loop completed

--- linkedList/ll.js
+++ linkedList/ll.js
@@ -1,3 +1,35 @@
-const abc = "";
-const xyz = "";
-const pqr = ""
+// const arr1 = [1, 2, [3, 4]];
+// arr1.flat();
+// // [1, 2, 3, 4]
+
+// const arr2 = [1, 2, [3, 4, [5, 6]]];
+// arr2.flat();
+// // [1, 2, 3, 4, [5, 6]]
+
+// const arr3 = [1, 2, [3, 4, [5, 6]]];
+// arr3.flat(2);
+// // [1, 2, 3, 4, 5, 6]
+
+
+const flatArr = (arr) => {
+    let result = [];
+    for(let i = 0; i < arr.length; i ++) {
+        if(typeof arr[i] ==  "object") {
+                result.push(...arr[i]);
+        } else result.push(arr[i]);
+    }
+    return result;
+};
+const flattenArr = (arr, num) => {
+    for(let i = 0; i < num; i ++) {
+        let copy = arr;
+        if(typeof arr[i] ==  "object") {
+            copy = flatArr(arr);
+        }
+        console.log(copy, 'result result');
+    }
+        // const res = flatArr(arr)
+};
+
+const sampleArr = [1, 2, [3, 4, [5, 6]]];
+flattenArr(sampleArr, 3);
--- linkedList/new_file.js
+++ linkedList/new_file.js
@@ -1,4 +1,13 @@
-const str = "abcavabcdab"
-
 // check the string contians A to Z and a to z
 // longest repeated frequency of alphabet
+// flat method 
+
+const arr = [2, 4, 2, 4, 22, 534, 2, 54, 232, 53];
+
+const sortArr = (arr) => {
+    for(let i = 0; i < arr.length; i ++) {
+        for(let j = 0; j < arr.length; j ++) {
+            if(arr[i]){}
+        }
+    }
+};
