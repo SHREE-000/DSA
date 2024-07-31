a92062b14974902aac37801368887a4180f8cae7
Shreehari C R <shreehari000@gamil.com>
2024-07-31 07:21:50 +0530

    Bubble sort

---
+++ algorithms/sort/bubble.js
@@ -0,0 +1,12 @@
+const bubbleSort = (arr) => {
+    for(let i = 0; i < arr.length; i ++) {
+        for(let j = 0; j < arr.length - i - 1; j ++) {
+            if(arr[j] > arr[j + 1]) [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
+        };
+    }
+    return arr;
+};
+
+const arr = [1, 2, 56, 1, 3, 4, 1, 11, 523 , 12];
+const result = bubbleSort(arr);
+console.log(result, "result result result");
